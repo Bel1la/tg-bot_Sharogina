28df91579ddfea2c3a640f7252db8378454510cd
Bel1la <92915421+Bel1la@users.noreply.github.com>
2022-10-07 20:06:12 +0500

    create f

---
+++ config.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+bot_token = "5549470260:AAH5eW_hqrzcyX9xh9obwyaEIxnAqJ1_MI8"
+
+api_Key="589f962b1856423b9c7467a2a6497704"
+
+# connect = sqlite3.connect('subsc.db', check_same_thread=False)
+# cursor = connect.cursor()
